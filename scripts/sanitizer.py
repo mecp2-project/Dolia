d758e5ac16cc82dff399ffdf0e9a84f39f382dfe
Daria Bogatova <daria@dbogatov.org>
2021-12-12 22:34:59 -0500

    rolling mean

--- scripts/sanitizer.py
+++ scripts/sanitizer.py
@@ -13,7 +13,7 @@ Inputs:
 		10. Verbosity for debugging.
 
 What it does:
-		This function reads CSV file, detects points with low likelihood and marks them as NaN and get removed. 
+		This function reads CSV file, detects points with low likelihood, marks them as NaN (not a number) and gets them removed. 
 		Next, it tries to fit an ellipse (ellipse.py).
 		If ellipse is found -- data is written into table, If ellipse not found data are aremoved.
 		Ratios of radii that are not within threshold range get removed.
@@ -47,22 +47,23 @@ def parse_cli():
 	def is_valid_percentile(parser, arg):
 		if not arg.isnumeric():
 			parser.error(f"Invalid value {arg}, is not a number")
-		a = float(arg)
-		if a > 1 or a < 0:
+		a = int(arg)
+		if a > 100 or a < 0:
 			parser.error(f"Invalid value {a}")
 		else:
-			return arg
+			return a
 	# All input that is needed
 	parser = argparse.ArgumentParser(description="Sanitizer (drop low likelihood, compute ellipses, interpolate radius ratio outliers and center coordinates)")
 	parser.add_argument("--file", dest="file", type=lambda x: is_valid_file(parser, x), required=True, help="CSV file to read.")
 	parser.add_argument("--likelihood", dest="likelihood", type=lambda x: is_valid_percentile(parser, x), default=0.9, help="Likelihood threshold. Datapoints below this will be dropped.")
-	parser.add_argument("--min-percentile", dest="min_percentile", type=lambda x: is_valid_percentile(parser, x), default=0.05, help="Min. percentile. Datapoints below this percentile will be dropped.")
-	parser.add_argument("--max-percentile", dest="max_percentile", type=lambda x: is_valid_percentile(parser, x), default=0.99, help="Max. percentile. Datapoints above this percentile will be dropped.")
-	parser.add_argument("--radius-max-percentile", dest="radius_max_percentile", type=lambda x: is_valid_percentile(parser, x), default=0.99, help="Max. percentile of a RADIUS. Datapoints above this percentile will be dropped.")
-	parser.add_argument("--radius-min-percentile", dest="radius_min_percentile", type=lambda x: is_valid_percentile(parser, x), default=0.01, help="Min. percentile of a RADIUS. Datapoints below this percentile will be dropped.")
+	parser.add_argument("--min-percentile", dest="min_percentile", type=lambda x: is_valid_percentile(parser, x), default= 1, help="Min. percentile. Datapoints below this percentile will be dropped.")
+	parser.add_argument("--max-percentile", dest="max_percentile", type=lambda x: is_valid_percentile(parser, x), default= 99, help="Max. percentile. Datapoints above this percentile will be dropped.")
+	parser.add_argument("--radius-max-percentile", dest="radius_max_percentile", type=lambda x: is_valid_percentile(parser, x), default= 99, help="Max. percentile of a RADIUS. Datapoints above this percentile will be dropped.")
+	parser.add_argument("--radius-min-percentile", dest="radius_min_percentile", type=lambda x: is_valid_percentile(parser, x), default=1, help="Min. percentile of a RADIUS. Datapoints below this percentile will be dropped.")
 	parser.add_argument("--eyeblink", dest="eyeblink", type=int, default=50, help="Number of pixels between top and bottom lid. Datapoints below this value will be dropped.")
 	parser.add_argument("-n", dest="n", type=int, default=0, help="Number of points to plot. Not plotting in default")
 	parser.add_argument("--window", dest="window", type=int, default=500, help="Window size in frames")
+	parser.add_argument("--rolling", dest="rolling", type=int, default=10, help="Rolling mead value")
 	parser.add_argument("-v", dest="verbose", default=False, help="increase output verbosity", action="store_true")
 
 	args = parser.parse_args()
@@ -73,7 +74,7 @@ def parse_cli():
 		datefmt='%a, %d %b %Y %H:%M:%S',
 	)
 
-	return args.file, args.likelihood, args.min_percentile, args.max_percentile, args.radius_max_percentile, args.radius_min_percentile, args.eyeblink, args.n, args.window
+	return args.file, args.likelihood, args.min_percentile, args.max_percentile, args.radius_max_percentile, args.radius_min_percentile, args.eyeblink, args.n, args.window, args.rolling
 
 
 def main():
@@ -81,7 +82,7 @@ def main():
 	import numpy as np
 	from ellipse import ellipse_fit
 
-	file, likelihood, min_percentile, max_percentile, radius_max_percentile, radius_min_percentile, eyeblink, n, window = parse_cli()
+	file, likelihood, min_percentile, max_percentile, radius_max_percentile, radius_min_percentile, eyeblink, n, window, rolling = parse_cli()
 
 
 	input = pd.read_csv(file, header=2, nrows=None if n == 0 else n)
@@ -116,20 +117,20 @@ def main():
 
 	frame = pd.DataFrame(initial_data, columns=['x0', 'y0', 'rlong', 'rshort', 'radius_ratio'])
 
-	# Remove radius outliers
-	frame[frame.radius_ratio > frame.radius_ratio.quantile(radius_max_percentile)] = np.nan 
-	frame[frame.radius_ratio < frame.radius_ratio.quantile(radius_min_percentile)] = np.nan
+	# Removes outliers with unacceptable ratio of radii
+	frame[frame.radius_ratio > frame.radius_ratio.quantile(radius_max_percentile/100.0)] = np.nan 
+	frame[frame.radius_ratio < frame.radius_ratio.quantile(radius_min_percentile/100.0)] = np.nan
 
 	#Sliding Window. Removes outliers
-	current = 0
+	current = 0 #first position
 	while current < len(frame.index):
 		current_window = min(window, len(frame.index) - current)
 		current_frame = frame[current:current + current_window].copy()
 
-		x0_max_percentile = current_frame.x0.quantile(max_percentile)
-		x0_min_percentile = current_frame.x0.quantile(min_percentile)
-		y0_max_percentile = current_frame.y0.quantile(max_percentile)
-		y0_min_percentile = current_frame.y0.quantile(min_percentile)
+		x0_max_percentile = current_frame.x0.quantile(max_percentile/100.0)
+		x0_min_percentile = current_frame.x0.quantile(min_percentile/100.0)
+		y0_max_percentile = current_frame.y0.quantile(max_percentile/100.0)
+		y0_min_percentile = current_frame.y0.quantile(min_percentile/100.0)
 
 		current_frame.loc[current_frame.x0 > x0_max_percentile, "x0"] = x0_max_percentile
 		current_frame.loc[current_frame.x0 < x0_min_percentile, "x0"] = x0_min_percentile
@@ -142,12 +143,18 @@ def main():
 
 	frame.interpolate(inplace=True) #interpolation
 
+	frame["roll_x0"] = frame["x0"].rolling(rolling).mean() #rolling mean to smooth the plot
+	
 	import matplotlib.pyplot as plt
 
-	plt.plot(frame["x0"], linewidth=0.5)
+	plt.plot(frame["roll_x0"], color = 'red', label = f'Rolling Mean {rolling}')
+	
+	plt.plot(frame["x0"], linewidth = 0.5, label = 'Raw Data')
 	plt.ylabel('Pixels')
 	plt.xlabel('Frames')
+	plt.legend()
 	plt.show()
+	
 
 
 if __name__ == "__main__":

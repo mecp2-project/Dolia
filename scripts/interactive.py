bcf6a28dd4cee99e6c2b509d4a2e3b0e0121d8e6
Daria Bogatova <daria@dbogatov.org>
2022-01-12 19:23:18 -0500

    Restructure.

--- scripts/interactive.py
+++ scripts/interactive.py
@@ -31,6 +31,8 @@ KEY_HOLD_NOT_SNAP = "alt"
 HORIZONTAL_TAG = "x0"
 VERTICAL_TAG = "y0"
 
+STD_COEFFICIENT = 2
+PEAK_SEARCH_DISTANCE = 50
 
 def parse_cli():
 
@@ -73,7 +75,7 @@ def update_peaks_file(peaks, peaks_file_path):
 	for tag in [HORIZONTAL_TAG, VERTICAL_TAG]:
 		peaks_as_lists[tag] = peaks[tag].tolist()
 
-	with open(peaks_path, "w", encoding='utf8') as peaks_file:
+	with open(peaks_path, "w", encoding="utf8") as peaks_file:
 		yaml.dump(peaks_as_lists, peaks_file, default_flow_style=False, allow_unicode=True)
 
 
@@ -102,14 +104,19 @@ def read_or_compute_peaks(frame, peaks_file_path):
 
 def main():
 
+	current_left_window_endpoint = 0
+	peak_selection = 0
+
+	background = None
+	active_peak_plots = {}
+	current_pressed_keys = []
+	current_mouse_x = 0
+
 	data_file, peaks_file, moving_avg, window = parse_cli()
 
 	frame = pd.read_csv(data_file)
 
-	std_coefficient = 2
-	peak_search_distance = 50
-
-	matplotlib.use('Qt5Agg')
+	matplotlib.use("Qt5Agg")
 
 	figure, (subplot_horizontal, subplot_vertical) = plt.subplots(2)
 
@@ -117,19 +124,12 @@ def main():
 		frame[f"mavg_{moving_avg}_{tag}"] = frame[tag].rolling(moving_avg).mean()
 
 		frame[f"std_{tag}"] = frame[f"mavg_{moving_avg}_{tag}"].rolling(moving_avg).std()
-		frame[f"std_plus_{tag}"] = frame[f"std_{tag}"] * std_coefficient + frame[f"mavg_{moving_avg}_{tag}"]
+		frame[f"std_plus_{tag}"] = frame[f"std_{tag}"] * STD_COEFFICIENT + frame[f"mavg_{moving_avg}_{tag}"]
 
 	peaks = read_or_compute_peaks(frame, peaks_file)
 
 	subplot_vertical.set_xlabel("Frames")
 	figure.text(0.06, 0.5, "Pixels", ha="center", va="center", rotation="vertical")
-
-	current = 0
-	peak = 0
-	peak_index = 0
-
-	background = None
-	active_peak_plots = {}
 
 	def redraw():
 		nonlocal background
@@ -138,18 +138,18 @@ def main():
 		for tag, subplot in [[HORIZONTAL_TAG, subplot_horizontal], [VERTICAL_TAG, subplot_vertical]]:
 			subplot.cla()
 
-			left_endpoint = max(0, current - int(0.5 * window))
-			right_endpoint = min(len(frame.index), current + window + int(0.5 * window))
+			left_endpoint = max(0, current_left_window_endpoint - int(0.5 * window))
+			right_endpoint = min(len(frame.index), current_left_window_endpoint + window + int(0.5 * window))
 			peaks_in_range = peaks[tag][(peaks[tag] >= left_endpoint) & (peaks[tag] <= right_endpoint)]
 
 			subplot.plot(frame[tag][left_endpoint:right_endpoint], linewidth=0.5, label="Original Sanitized Series", color="darkslategray")
-			subplot.plot(frame[f"std_plus_{tag}"][left_endpoint:right_endpoint], label=f"{std_coefficient} STD from {moving_avg} moving average", color="teal")
+			subplot.plot(frame[f"std_plus_{tag}"][left_endpoint:right_endpoint], label=f"{STD_COEFFICIENT} STD from {moving_avg} moving average", color="teal")
 			subplot.plot(peaks_in_range, frame[f"std_plus_{tag}"][peaks_in_range], "o", color="orange", alpha=0.5)
 
 			(active_peak_plots[tag], ) = subplot.plot([], [], marker="o", color="red", alpha=0.75, animated=True)
 
-			subplot.set_xlim(current, current + window)
-			subplot.set_ylim(frame[tag][current:current + window].min() * 0.9, frame[tag][current:current + window].max() * 1.1)
+			subplot.set_xlim(current_left_window_endpoint, current_left_window_endpoint + window)
+			subplot.set_ylim(frame[tag][current_left_window_endpoint:current_left_window_endpoint + window].min() * 0.9, frame[tag][current_left_window_endpoint:current_left_window_endpoint + window].max() * 1.1)
 
 			subplot.set_title(f"{title} Movements")
 			subplot.legend()
@@ -157,52 +157,43 @@ def main():
 		figure.canvas.draw()
 		background = figure.canvas.copy_from_bbox(figure.bbox)
 
-	redraw()
-
-	current_pressed_keys = []
-
 	def key_release_handler(event):
 		if event.key in current_pressed_keys:
 			current_pressed_keys.remove(event.key)
 		logger.debug(f"Released {event.key}, current_pressed_keys: {current_pressed_keys}")
 
 	def key_press_handler(event):
-		nonlocal current
+		nonlocal current_left_window_endpoint
 		nonlocal window
-		nonlocal peak_index
 		nonlocal current_pressed_keys
 
 		logger.debug(f"key pressed: {event.key}")
 		current_pressed_keys += [event.key]
 
 		if event.key == KEY_START:
-			current = 0
+			current_left_window_endpoint = 0
 		elif event.key == KEY_RIGHT:
-			current += int(window * 0.8)
-			current = min(current, len(frame.index) - 1)
+			current_left_window_endpoint += int(window * 0.8)
+			current_left_window_endpoint = min(current_left_window_endpoint, len(frame.index) - 1)
 		elif event.key == KEY_LEFT:
-			current -= int(window * 0.8)
-			current = max(current, 0)
+			current_left_window_endpoint -= int(window * 0.8)
+			current_left_window_endpoint = max(current_left_window_endpoint, 0)
 		elif event.key == KEY_ZOOM_IN:
 			window = int(window / 2)
 		elif event.key == KEY_ZOOM_OUT:
 			window = int(window * 2)
 		redraw()
 
-	current_mouse_x = 0
-
 	def motion_notify_handler(event):
 		nonlocal current_mouse_x
-		nonlocal peak
+		nonlocal peak_selection
 
 		def compute_nearest_peak(tag, current):
-			nonlocal peak
-
 			peak = current
 			if KEY_HOLD_NOT_SNAP in current_pressed_keys:
 				return peak
 
-			points = frame[f"std_plus_{tag}"][current:min(len(frame.index), current + peak_search_distance)].tolist()
+			points = frame[f"std_plus_{tag}"][current:min(len(frame.index), current + PEAK_SEARCH_DISTANCE)].tolist()
 			for x in range(0, len(points) - 2):
 				peak = x + current
 				if peak in peaks[tag]:
@@ -219,10 +210,10 @@ def main():
 			if event.inaxes == subplot:
 				if current_mouse_x != event.xdata:
 					current_mouse_x = event.xdata
-					peak = compute_nearest_peak(tag, int(event.xdata))
+					peak_selection = compute_nearest_peak(tag, int(event.xdata))
 
-					active_peak_plots[tag].set_data([peak], [frame[f"std_plus_{tag}"][peak]])
-					if peak in peaks[tag]:
+					active_peak_plots[tag].set_data([peak_selection], [frame[f"std_plus_{tag}"][peak_selection]])
+					if peak_selection in peaks[tag]:
 						active_peak_plots[tag].set(marker="x")
 					else:
 						active_peak_plots[tag].set(marker="o")
@@ -239,12 +230,12 @@ def main():
 		if not event.dblclick and event.button == 1:
 			for tag, subplot in [[HORIZONTAL_TAG, subplot_horizontal], [VERTICAL_TAG, subplot_vertical]]:
 				if event.inaxes == subplot:
-					if peak in peaks[tag]:
-						peaks[tag] = np.delete(peaks[tag], peaks[tag] == peak)
-						logger.debug(f"Unconfirmed peak: {peak}")
+					if peak_selection in peaks[tag]:
+						peaks[tag] = np.delete(peaks[tag], peaks[tag] == peak_selection)
+						logger.debug(f"Removed peak: {peak_selection}")
 					else:
-						peaks[tag] = np.append(peaks[tag], peak)
-						logger.debug(f"Confirmed peak: {peak}")
+						peaks[tag] = np.append(peaks[tag], peak_selection)
+						logger.debug(f"Added peak: {peak_selection}")
 					update_peaks_file(peaks, peaks_file)
 					redraw()
 					break
@@ -252,7 +243,10 @@ def main():
 	figure.canvas.mpl_connect("motion_notify_event", motion_notify_handler)
 	figure.canvas.mpl_connect("key_press_event", key_press_handler)
 	figure.canvas.mpl_connect("key_release_event", key_release_handler)
-	figure.canvas.mpl_connect('button_press_event', on_click_handler)
+	figure.canvas.mpl_connect("button_press_event", on_click_handler)
+	figure.canvas.mpl_connect("resize_event", lambda event: redraw())
+
+	redraw()
 
 	plt.show()
 

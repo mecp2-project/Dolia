0f36da649e337a1db6f6871cd9fd7c6aa7e0c1b9
Daria Bogatova <daria@dbogatov.org>
2023-06-16 12:05:06 -0400

    locomotion adjustment

--- scripts/locomotion.py
+++ scripts/locomotion.py
@@ -52,7 +52,7 @@ def parse_cli():
 	# All input that is needed
 	parser = argparse.ArgumentParser(description="Sanitizer (drop low likelihood and high percentile, crop CSV, calculate locomotion")
 	parser.add_argument("--file", dest="file", type=lambda x: is_valid_file(parser, x), required=True, help="CSV file to read.")
-	parser.add_argument("--category-file", dest="category_file", type=lambda x: is_valid_file(parser, x), required=True, help="CSV file to read.")
+	parser.add_argument("--category-file", dest="category_file", type=lambda x: is_valid_file(parser, x), help="CSV file to read.")
 	parser.add_argument("-v", dest="verbose", default=False, help="increase output verbosity", action="store_true")
 
 	args = parser.parse_args()
@@ -74,9 +74,6 @@ def main():
 	# Parse the CSV files.
 	file, category_file = parse_cli()
 	
-	category_frame = pd.read_csv(category_file)
-	logging.info(f"Parsed Category CSV (n = {len(category_frame.index)})")
-
 	frame = pd.read_csv(file, header=2)
 
 	logging.info(f"Parsed CSV (n = {len(frame.index)})")
@@ -118,32 +115,38 @@ def main():
 			current += INCREMENT
 			pbar.update(INCREMENT)
 
-	logging.info(f"Found {len(movements)} movement windows")
-
 	total_frames = 0
+	logging.info(f"Found {len(movements)} movement windows")
+	for start, end in movements:
+			logging.info(f"Movement [{start} : {end}] {end - start} frames")
+			total_frames += end - start
+
+	
 	switches_during_movement = 0
 
-	for start, end in movements:
-		logging.info(f"Movement [{start} : {end}] {end - start} frames")
-		total_frames += end - start
+	if category_file is not None:
+		category_frame = pd.read_csv(category_file)
+		logging.info(f"Parsed Category CSV (n = {len(category_frame.index)})")
+		
+		
 		switches_during_movement += sum((category_frame["start"]/5 >= start) & (category_frame["start"]/5 < end))
 
-	logging.info(f"Total frames where mouse walks: {total_frames}")
-	logging.info(f"Switches occurred during locomotion: {switches_during_movement}")
-	# Calculate percentage of switches during locomotion
-	total_switches = len(category_frame["start"])
-	percentage_during_movement = (switches_during_movement / total_switches) * 100
+		logging.info(f"Total frames where mouse walks: {total_frames}")
+		logging.info(f"Switches occurred during locomotion: {switches_during_movement}")
+		# Calculate percentage of switches during locomotion
+		total_switches = len(category_frame["start"])
+		percentage_during_movement = (switches_during_movement / total_switches) * 100
 
-	logging.info(f"Percentage of switches during locomotion: {percentage_during_movement}%")
+		logging.info(f"Percentage of switches during locomotion: {percentage_during_movement}%")
 
-	#ax = frame.iloc[:, C_LEFT_PAW_X].plot(label="Left Paw")
-	#frame.iloc[:, C_RIGHT_PAW_X].plot(ax=ax, label="Right Paw")
-	#[plt.axvline(start/5, linewidth=1, color='r') for start in category_frame["start"]]
-	#plt.xlabel("Frames")
-	#plt.ylabel("Pixels")
-	#plt.legend()
+	ax = frame.iloc[:, C_LEFT_PAW_X].plot(label="Left Paw")
+	frame.iloc[:, C_RIGHT_PAW_X].plot(ax=ax, label="Right Paw")
+	#[plt.axvline(start/5, linewidth=1, color='r') for start in category_frame["start"]]		#if Category_file is not None -- uncomment
+	plt.xlabel("Frames")
+	plt.ylabel("Pixels")
+	plt.legend()
 	
-	#plt.show()
+	plt.show()
 
 if __name__ == "__main__":
 	main()
